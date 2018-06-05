f433b335d193e5c609b8d82557c8e67c248c9b1a
phyuzinzaw <phyuzinzaw644@gmail.com>
2018-06-05 14:27:23 +0630

    change

---
+++ ex1.py
@@ -0,0 +1,8 @@
+print("hello world")
+print("hello again")
+print("i like typing this")
+print("this is fun")
+print("yay! printing")
+print("i'd such rather you not")
+print('i "said" do not touch this')
+
