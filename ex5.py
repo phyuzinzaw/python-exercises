3ada56d57207f68b0ce683457137b6963df85eeb
phyuzinzaw <phyuzinzaw644@gmail.com>
2018-06-05 22:56:00 +0630

    add ex5.py

--- ex5.py
+++ ex5.py
@@ -1,2 +1,15 @@
-print:
-
+my_name='phyuzinzaw'
+my_age=21
+my_height=74
+my_weight=100
+my_eyes='blue'
+my_teeth='white'
+my_hair='brown'
+print(f"let's talk about {my_name}.")
+print(f"she's {my_height}inches tall.")
+print(f"she's {my_weight} pounds heavy.")
+print(f"actually that's not too heavy.")
+print(f"his teeth are usually {my_teeth} depending on the coffee.")
+print(f"she's got {my_eyes} eyes and {my_hair}hair.")
+total=my_age+my_height+my_weight
+print(f"if i add {my_age},{my_height},and {my_weight} i get {total}.")
