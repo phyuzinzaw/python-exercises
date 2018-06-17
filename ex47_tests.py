b566ed551e81d045556ea0e99103fea33ec3dbb6
phyuzinzaw <phyuzinzaw644@gmail.com>
2018-06-17 23:41:38 +0630

    add ex47

---
+++ ex47_tests.py
@@ -0,0 +1,17 @@
+from nose.tools import *
+from ex47.game import Room
+
+
+def test_room():
+     gold = Room("GoldRoom",
+            """This room has gold in it you can grab. There's a
+            door to the north.""")
+    assert_equal(gold.name, "GoldRoom")
+    assert_equal(gold.paths, {})
+
+def test_room_paths():
+        center = Room("Center", "Test room in the center.")
+        north = Room("North", "Test room in the north.")
+        south = Room("South", "Test room in the south.")
+
+        center.add_paths({'north': north, 'south': south})
