24209c398de951305840d0e0da44e9d0b5d3e1a1
phyuzinzaw <phyuzinzaw644@gmail.com>
2018-06-18 10:12:01 +0630

    add app.py

--- app.py
+++ app.py
@@ -6,8 +6,13 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     greeting = "Hello World"
-    return render_template("index.html", greeting=greeting)
-
-if __name__ == "__main__":
+    if request.method =="POST":
+        name=request.form['name']
+        greet =request.form['greet']
+        greeting=f"{greet},{name}"
+        return render_template("index.html",greeting=greeting)
+    else:
+        return render_template("hello_form.html")
+if__name__ == "__main__":
     app.run()
 
